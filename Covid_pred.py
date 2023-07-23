fb0b98ddb6964720661bc8634e006083091959c4
Abdul Hamith <129108121+ham8925@users.noreply.github.com>
2023-07-23 18:49:40 +0530

    Update Covid_pred.py

--- Covid_pred.py
+++ Covid_pred.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.image('Covid 1.jpg')
 st.markdown("## Object Classifier App with Deep Learning")
 st.markdown("""
-This app uses Deep learning (ResNet50) libraries namely keras to identify objects from images.
+This app uses Deep learning (MobileNet) libraries namely keras to identify objects from images.
 
-ResNet-50 is a convolutional neural network that is 50 layers deep. You can load a pretrained version of the network trained on more than a million images from the ImageNet database. The pretrained network can classify images into 1000 object categories, such as keyboard, mouse, pencil, and many animals.
+MobileNet is a convolutional neural network that is 50 layers deep. You can load a pretrained version of the network trained on more than a million images from the ImageNet database. The pretrained network can classify images into 1000 object categories, such as keyboard, mouse, pencil, and many animals.
 
 **Made by Wisecorner Labs**
 
