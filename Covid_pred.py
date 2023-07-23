105ba91bc81c2975def8538d57b6499eb91e7aab
Abdul Hamith <129108121+ham8925@users.noreply.github.com>
2023-07-23 18:48:39 +0530

    Update Covid_pred.py

--- Covid_pred.py
+++ Covid_pred.py
@@ -7,6 +7,9 @@ This app uses Deep learning (ResNet50) libraries namely keras to identify object
 
 ResNet-50 is a convolutional neural network that is 50 layers deep. You can load a pretrained version of the network trained on more than a million images from the ImageNet database. The pretrained network can classify images into 1000 object categories, such as keyboard, mouse, pencil, and many animals.
 
-**Made by Ifeanyi Nneji**
+**Made by Wisecorner Labs**
 
 """)
+
+object_image = st.file_uploader("Upload an image...", type=['png','jpg','webp','jpeg'])
+submit = st.button('Predict')
